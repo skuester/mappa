8b15beca8086a52d62b92b8768eb0fc8d4797a72
Shane Kuester <shout@shanekuester.com>
2016-03-27 23:45:01 -0500

    Adds first example

--- mappa.js
+++ mappa.js
@@ -1,6 +1,56 @@
+var _get = require('lodash/get')
+var _set = require('lodash/set')
+// TODO: Object.keys shim
 module.exports = Mappa
 
 
-function Mappa() {
+function Mappa(config) {
+	var read_ops = []
+	var write_ops = []
 
+
+	init(config)
+
+
+	return {
+		read: read,
+		write: write
+	}
+
+
+
+
+	function read(source) {
+		var obj = {}
+		read_ops.forEach(function (op) {
+			op(source, obj)
+		})
+		return obj
+	}
+
+
+	function write(obj) {
+		var source = {}
+		write_ops.forEach(function (op) {
+			op(obj, source)
+		})
+		return source
+	}
+
+
+	// private
+
+
+	function init(config) {
+		Object.keys(config).forEach(function (key) {
+
+			read_ops.push(function (source, obj) {
+				_set(obj, key, config[key]._read(source) )
+			})
+
+			write_ops.push(function (obj, source) {
+				config[key]._write(_get(obj, key), source)
+			})
+		})
+	}
 }
--- mappa.spec.js
+++ mappa.spec.js
@@ -1,11 +1,31 @@
-var Mappa = require('./mappa')
+var Mapper = require('./mappa')
 var expect = require('chai').expect
 
 
 describe ("Mappa", function () {
 
-	it ("works", function () {
-		expect( false ).to.be.false
+	it ("maps an object key with a _read and _write function", function () {
+		var mapper = Mapper({
+			name: {
+				_read: function (source) {
+					return source.OldName
+				},
+				_write: function (value, source) {
+					source.OldName = value
+				}
+			}
+		})
+
+		var source = {
+			OldName: 'OldName Value',
+		}
+
+		var result = {
+			name: 'OldName Value'
+		}
+
+		expect( mapper.read(source) ).to.eql( result )
+		expect( mapper.write(result) ).to.eql( source )
 	});
 
 });
