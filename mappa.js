19bedd698b9b4ee05c9764485e97c0272b62ca84
Shane Kuester <shout@shanekuester.com>
2016-03-27 22:04:38 -0500

    Initial Commit

---
+++ mappa.js
@@ -0,0 +1,6 @@
+module.exports = Mappa
+
+
+function Mappa() {
+
+}
---
+++ mappa.spec.js
@@ -0,0 +1,11 @@
+var Mappa = require('./mappa')
+var expect = require('chai').expect
+
+
+describe ("Mappa", function () {
+
+	it ("works", function () {
+		expect( false ).to.be.false
+	});
+
+});
