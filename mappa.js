48c03bf8da758fb6040b15e54dc6269f0c4069a8
Shane Kuester <shout@shanekuester.com>
2018-01-18 12:04:43 -0600

    remove old comments

--- mappa.js
+++ mappa.js
@@ -3,14 +3,14 @@
 var Mapper = require('./lib/mapper')
 var MapperRegistry = require('./lib/mapper-registry')
 
-// var Helper = require('./lib/helper')
-
-// TODO: Object.keys shim
 module.exports = Mappa
+
+
 
 
 function Mappa(opts) {
 	return new Mapper(opts)
 }
+
 
 Mappa.Registry = MapperRegistry
